fe1e831e8fe0437e75f15ac214e66124d70fda58
Raman Adlakha <radlakha@users.noreply.github.com>
2024-02-21 07:14:43 +0000

    Allow spaces in Incomplete syntax

--- src/rules/checkTodoSyntax.ts
+++ src/rules/checkTodoSyntax.ts
@@ -13,7 +13,7 @@ import { nodeToString } from "@/util/nodeToString";
  *	%% TODO %%
  */
 
-const syntaxRegex = /%%\s*TODO(?:\(([^)]+)\))?\s*%%/g;
+const syntaxRegex = /%%\s*TODO\s*(?:\(([^)]+)\))?\s*%%/g;
 
 export const checkTodoSyntax: IssueScanner = {
 	icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list-check"><path d="M5 12l5 5L19 6"/></svg>`,
